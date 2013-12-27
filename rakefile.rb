cccd210c5c8e106d65057ec0ecaff3efbba953d7
Adam Ralph <adam@adamralph.com>
2013-12-27 17:08:02 +0000

    initial commit

---
+++ rakefile.rb
@@ -0,0 +1,51 @@
+require 'albacore'
+
+xunit_command_net40 = "src/packages/xunit.runners.1.9.2/tools/xunit.console.clr4.exe"
+nuget_command = "src/packages/NuGet.CommandLine.2.7.3/tools/NuGet.exe"
+solution = "src/XBehave.Samples.sln"
+
+samples = [
+  { :command => xunit_command_net40, :assembly => "src/Xbehave.Samples.Net40/bin/Debug/Xbehave.Samples.Net40.dll" }
+]
+
+Albacore.configure do |config|
+  config.log_level = :verbose
+end
+
+desc "Execute default tasks"
+task :default => [:sample]
+
+desc "Restore NuGet packages"
+exec :restore do |cmd|
+  cmd.command = nuget_command
+  cmd.parameters "restore #{solution}"
+end
+
+desc "Clean solution"
+msbuild :clean do |msb|
+  msb.properties = { :configuration => :Release }
+  msb.targets = [:Clean]
+  msb.solution = solution
+end
+
+desc "Build solution"
+msbuild :build => [:clean, :restore] do |msb|
+  msb.properties = { :configuration => :Release }
+  msb.targets = [:Build]
+  msb.solution = solution
+end
+
+desc "Execute samples"
+task :sample => [:build] do
+  execute_xunit samples
+end
+
+def execute_xunit(tests)
+  tests.each do |test|
+    xunit = XUnitTestRunner.new
+    xunit.command = test[:command]
+    xunit.assembly = test[:assembly]
+    xunit.options "/html", test[:assembly] + ".TestResults.html", "/xml", test[:assembly] + ".TestResults.xml"
+    xunit.execute  
+  end
+end
---
+++ readme.md
@@ -0,0 +1,15 @@
+![xBehave.net](https://raw.github.com/xbehave/xbehave.net/master/assets/xbehave_128x128.png)
+
+# Samples
+
+Sample test projects demonstrating features of [xBehave.net](http://xbehave.github.io/).
+
+## Can I help out? ##
+
+Absolutely! Please feel free to raise issues, fork the source code, send pull requests, etc.
+
+No pull request is too small. Even whitespace fixes are appreciated. Before you contribute anything make sure you read [CONTRIBUTING.md](https://github.com/xbehave/xbehave.net-samples/blob/master/CONTRIBUTING.md).
+
+Come and chat to fellow users and developers at the [xBehave.net JabbR chat room](http://jabbr.net/#/rooms/xbehavenet).
+
+xBehave.net logo designed by Vanja Pakaski.
