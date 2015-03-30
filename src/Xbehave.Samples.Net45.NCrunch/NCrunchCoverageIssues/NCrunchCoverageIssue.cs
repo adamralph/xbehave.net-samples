25db3fcee5686a5e69a3847ab621d4546392b9b3
Adam Ralph <adam@adamralph.com>
2015-03-30 08:54:43 +0200

    updated NCrunch coverage issues WRT NCrunch build 2.14.0.5

---
+++ src/Xbehave.Samples.Net45.NCrunch/NCrunchCoverageIssues/NCrunchCoverageIssue.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="NCrunchCoverageIssue.cs" company="xBehave.net contributors">
+//  Copyright (c) xBehave.net contributors. All rights reserved.
+// </copyright>
+
+namespace Xbehave.Samples.NCrunchCoverageIssues
+{
+    using FluentAssertions;
+    using Xbehave;
+
+    // As per NCrunch preview build 2.14.0.5, this is the only identified coverage issue.
+    // Note that if the pass/fail indicators are assumed to be set for a scenario as whole then they are correct.
+    // They are only 'incorrect' if the expectation is that they are set independently for each step.
+    public class NCrunchCoverageIssue
+    {
+        [Scenario]
+        public void OnePassingStepAndOneFailingStep(int answer)
+        {
+            // NCrunch marks the step definition as failing - INCORRECT
+            // NCrunch marks the step delegate as failing - INCORRECT
+            "Given true is true"
+                .f(() => true.Should().BeTrue());
+
+            // NCrunch marks the step definition as failing - INCORRECT
+            // NCrunch marks the step delegate as failing - CORRECT
+            // NCrunch marks the second statement of the delegate as the exception source - CORRECT
+            "Then true is false"
+                .f(() =>
+                {
+                    true.Should().BeTrue();
+                    true.Should().BeFalse();
+                });
+
+            // NCrunch marks the step definition as failing - INCORRECT
+            // NCrunch marks the step delegate as uncovered - CORRECT
+            "And true is true"
+                .f(() => true.Should().BeTrue());
+        }
+    }
+}
